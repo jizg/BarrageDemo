303998514297ebd0f29ad363af1cdb53cda66a4b
Zhigang Ji <jizg@cn.ibm.com>
2020-06-05 22:06:09 +0800

    use CommentCoreLibrary for displaying danmu

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,8 +1,14 @@
+var cm = new CommentManager(document.getElementById('commentCanvas'));
+cm.init(); // 初始化
+
 $(document).ready(function () {
+
+		loadDynamicSource();
+		// cm.start();
 
         var button = $("#button");
         var inputBox = $(".inpu-box");
-        var barrageBox = $(".barrage-box");
+        // var barrageBox = $(".barrage-box");
 
         //输入框获取焦点的事件/删除提示字
         inputBox.focus(function () {
@@ -12,7 +18,7 @@ $(document).ready(function () {
 
         // 点击发送按钮后
         button.click(function () {
-            websocket.send(inputBox.text());       //发送输入框的文本信息
+            websocket.send(bilibiliFormater(inputBox.text()));       //发送输入框的文本信息
             //发送后删除输入框消息
             inputBox.empty();
         });
@@ -20,7 +26,7 @@ $(document).ready(function () {
         inputBox.keypress(function (event) {
             if (event.which == 13) {
                 event.preventDefault();     //给回车事件一个默认函数,防止自动换行
-                websocket.send(inputBox.text());
+                websocket.send(bilibiliFormater(inputBox.text()));
                 //发送后删除输入框消息
                 inputBox.empty();
             }
@@ -30,27 +36,9 @@ $(document).ready(function () {
         //发送弹幕逻辑------------------------------------------------
         websocket.onmessage = function (event) {
 
-            var msgtxt = event.data; //获取弹幕消息
-
-            var colorRandom = getRandomColor();//得到随机颜色
-
-            var num = generateRandom(3);//得到随机的三位数
-            var toppx = num % 300; //得到距离顶端距离(0-300)
-
-            //给新弹幕<p>添加样式和内容
-            var newTxt = '<p style="top:' + toppx + 'px; color:' + colorRandom + '">' + msgtxt + '</p>';
-
-            barrageBox.append(newTxt); //将新弹幕<p>加到弹幕盒子中
-            var newBarrage = barrageBox.find("p"); //获取新弹幕元素
-
-            var pWidth = newBarrage.width();  //得到新弹幕元素的宽度
-            newBarrage.css("right", "-" + pWidth + "px"); //初始位置在弹幕盒子右边外面
-
-            //然后添加右移动画效果 最后消失
-            newBarrage.animate({right: "666px"}, 5000, "linear", function () {
-                $(this).remove();
-            });
-
+			if (event.data) {
+				cm.send(bilibiliParser(event.data));
+			}
         }
 
         //随机颜色
@@ -71,55 +59,41 @@ $(document).ready(function () {
         }
     }
 );
-/*
-window.addEventListener('load', function(){
-    var CM = new CommentManager(document.getElementById('my-comment-stage'));
-    CM.init(); // 初始化
 
-    // 启动播放弹幕（在未启动状态下弹幕不会移动）
-    CM.start();
-    // 绑定按钮们
-    document.getElementById('btnLoadTimeline').addEventListener('click', function(e){
-        e.preventDefault(); // 抑制默认操作
-        var danmakuTimeline = [
-            {
-                "mode":1,
-                "text":"Hello World",
-                "stime":0,
-                "size":25,
-                "color":0xffffff
-            }
-        ];
-        CM.load(danmakuTimeline);
-    });
-    
-    document.getElementById('btnInsertTimeline').addEventListener('click', function(e){
-        e.preventDefault(); // 抑制默认操作
-        var danmaku = {
-            "mode":1,
-            "text":"Hello CommentCoreLibrary",
-            "stime":1000,
-            "size":30,
-            "color":0xff0000
-        };
-        CM.insert(danmaku);
-    });
-    
-    var startTime = 0, iVal = -1;
-    document.getElementById('btnTimer').addEventListener('click', function(e){
-        e.preventDefault(); // 抑制默认操作
-        startTime = Date.now(); // 设定起始时间
-        if(iVal >= 0){
-            clearInterval(iVal); // 如果之前就有定时器，把它停掉
-        }
-        //建立新的定时器
-        iVal = setInterval(function(){
-            var playTime = Date.now() - startTime; // 用起始时间和现在时间的差模拟播放
-            CM.time(playTime); // 通报播放时间
-            document.getElementById('txPlayPos').textContent = playTime; // 显示播放时间
-        }, 100); // 模拟播放器每 100ms 通报播放时间
-    });
-    
-    // 开放 CM 对象到全局这样就可以在 console 终端里操控
-    window.CM = CM;
-}); */
+function loadDynamicSource() {
+	var provider = new CommentProvider();
+
+	// 绑定动态源，动态源可以是两种：LongPoll 或者 EventDispatcher
+	provider.addDynamicSource(websocket, CommentProvider.SOURCE_XML);
+	// 会使用 parseOne 来逐个解析动态弹幕
+	provider.addParser(new BilibiliFormat.XMLParser(), CommentProvider.SOURCE_XML);
+
+	provider.addTarget(cm);
+
+	cm.start();
+}
+var index = 0;
+function bilibiliFormater(comment) {
+	var templates = [
+		"<?xml version=\"1.0\" encoding=\"UTF-8\"?><i><chatserver>chat.bilibili.tv</chatserver><chatid>170102</chatid><d p=\"94,1,25,16777215,1307940958,0,Da9e216f,30723621\">{comment}</d></i>",
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><i><chatserver>chat.bilibili.tv</chatserver><chatid>170102</chatid><d p=\"1.9500000476837,7,12,0,1304950431,1,b82a51a3,26514873\">[\"162\",\"285\",\"1-0\",\"6\",{comment},\"0\",\"0\",\"161\",\"360\",\"6000\",\"0\"]</d></i>",
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><i><chatserver>chat.bilibili.tv</chatserver><chatid>170102</chatid><d p=\"62.5,1,25,39423,1309764903,0,D7db7038,34406385\">{comment}</d></i>",
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><i><chatserver>chat.bilibili.tv</chatserver><chatid>170102</chatid><d p=\"44.049999237061,7,36,16777215,1304950431,1,b82a51a3,26515093\">[\"194\",\"178\",\"0-1\",\"4.0\",\"{comment}\"]</d></i>",
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><i><chatserver>chat.bilibili.tv</chatserver><chatid>170102</chatid><d p=\"86.800003051758,4,18,16711680,1316669406,0,1086a4ae,51534405\">{comment}</d></i>",
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><i><chatserver>chat.bilibili.tv</chatserver><chatid>170102</chatid><d p=\"109.15000152588,7,64,16777215,1304950431,1,b82a51a3,26515466\">[\"78\",\"383\",\"1-0\",\"3.5\",\"{comment}\",\"0\",\"30\",\"20\",\"130\",\"3500\",\"0\"]</d></i>",
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><i><chatserver>chat.bilibili.tv</chatserver><chatid>170102</chatid><d p=\"74.800003051758,4,18,0,1307879028,0,96645f00,30843692\">{comment}</d></i>"
+    ];
+    if (index > templates.length - 1) {
+        index = 0
+    }
+	return templates[index++].replace("{comment}", comment)
+}
+
+function bilibiliParser(comment) {
+	xmlParser = new DOMParser();
+
+	var bparser = new BilibiliFormat.XMLParser();
+	
+	var formattedComment = xmlParser.parseFromString(comment,"text/xml")
+	return bparser.parseOne(formattedComment.getElementsByTagName('d')[0]);
+}
--- src/main/resources/templates/index/index.ftl
+++ src/main/resources/templates/index/index.ftl
@@ -5,16 +5,33 @@
     <title></title>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/dist/style.css">
+    <style>
+pre{margin:0;}
+pre.error{color:#f00;}
+pre.warning{color:#FFC500;}
+.s-button, .abp .button{
+	display:block; border:1px solid #f88; padding:8px 6px 8px 6px; 
+	background:#000; color:#f88; float:left;
+	-moz-user-select: none;
+	-khtml-user-select: none; 
+	-webkit-user-select: none; 
+	-o-user-select: none; 
+	user-select: none; 
+	cursor:default;
+}
+.s-button:hover,.abp .button:hover{background:#f88; color:#000;}
+</style>
     <script src="https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="/dist/CommentCoreLibrary.js"></script>
+    <script src="/dist/scripting/Host.js"></script>
 </head>
 <body>
-
+<div style="position:absolute;right:0; top:0; width: 45%; overflow:auto; height:384px; padding:3px;" id="debug-console"></div>
 
 <div class="bigbox">
     <div class="head-box"></div>
     <div class="content-box">
-        <div class="vid-box">
+        <!--<div class="vid-box">
             <video class="pause" poster="/img/timg.jpg" width="100%" height="auto">
                 <source src="/video/Pigv3.mp4" type="video/mp4">
                 您的浏览器不支持html5！
@@ -22,11 +39,20 @@
             <div class="barrage-box">
             </div>
             <div class="play-button"><img class="play-img" src="/img/play.png"/></div>
+        </div>-->
+        <div id="player-unit" style="width:100%;clear:both;background:#000">
+            <div class="m20 abp" id="player" style="height:375px">
+                <div id="commentCanvas" class="container"></div>
+                <video id="abpVideo" autobuffer="true" data-setup="{}" width="100%" height="100%" style="z-index:-1;width:100%;height:100%;top:0;left:0;right:0;bottom:0;" preload="auto">
+                </video>
+                
+            </div>
+            <div style="clear:both;"></div>
         </div>
 
         <div class="inpu-box" contenteditable=true><p>这里输入弹幕</p></div>
         <div id="button" class="button"><img class="send-img" src="/img/send.png" alt="发送"/></div>
-
+        
         
         </div>
 
